38e1a7cb9a53e8bc0c35ec115b239a717ee4cd0b
Harshit Tripathi <htripathi9324@gmail.com>
2026-01-28 21:49:27 +0530

    Day 1 and Day 2

---
+++ Day 1/first.py
@@ -0,0 +1,8 @@
+print("Hello")
+print("World")
+
+name_adad = "a"
+print(name_adad)
+
+name = input("Enter Your Name: ")
+print(name)
---
+++ Day 2/P_S_1.py
@@ -0,0 +1,41 @@
+# #Q.1
+name = "Harshit"
+age = 20
+address = "Mumbai"
+
+print(name,"\n", age,"\n", address,"\n" )
+
+# #Q.2
+a = 10
+b = 20
+
+temp = a
+a=b
+print(a)
+
+b = temp
+print(b)
+
+#Q.3
+a = 12.6
+print(type(a))
+a = int(a)
+print(type(a),a)
+
+# Q.4
+name = input("Enter Your Name: ")
+Phone_No = int(input("Enter Your Phone No: "))
+Address = input("Enter Your Address: ")
+Uid = int(input("Enter Your Uid: "))
+
+print("\n")
+print("Your Name is :",name)
+print("Your Phone No is :",Phone_No)
+print("Your Address is :",Address)
+print("Your Uid is :",Uid)
+
+
+#Q.5
+user = int(input("Enter a Number: "))
+user = float(user)
+print(type(user))
