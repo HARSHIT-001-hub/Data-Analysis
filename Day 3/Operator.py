5555d09c5fb7e0a938291acffb6a4fe629830773
Harshit Tripathi <htripathi9324@gmail.com>
2026-01-29 20:18:57 +0530

    Create Operator.py

---
+++ Day 3/Operator.py
@@ -0,0 +1,18 @@
+#1. Arithmetic
+print (2 + 3)
+print(2 % 3)
+
+#2. Comparision
+print(2 > 3)
+print(2 != 3)
+
+#3. Logical
+print(3>4 or 3<4)
+
+#4. Identify
+a=1234
+b="1234"
+c=1234
+print(a is b)
+print(a is c)
+
