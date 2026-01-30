4410a4f8e7a0e64a65e49f2e69b27c51f41dae3c
Harshit Tripathi <htripathi9324@gmail.com>
2026-01-30 18:44:23 +0530

    create loops

---
+++ Day 4/P_S_3.py
@@ -0,0 +1,23 @@
+# sum = 0
+# for i in range (0,51):
+#     if i % 2 == 0:
+#         sum += i
+#         print(sum)
+
+# num = 0
+# for i in range (1,21):
+#     print("Square of ",i,"is:",i*2)
+
+# sum = 0
+# i = 0
+# while i <= 20:
+#     if i % 2 != 0:
+#         sum += i
+#     i += 1
+# print(sum)
+
+# for i in range (1,101):
+#     if i%8 ==0 and i%12 == 0 :
+#         print(i)
+# Correct
+
