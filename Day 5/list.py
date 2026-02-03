7d1548b041c970d44fd29a1f0fb973867653d931
Harshit Tripathi <htripathi9324@gmail.com>
2026-02-03 19:51:18 +0530

    day 5

---
+++ Day 5/list.py
@@ -0,0 +1,2 @@
+a = ["Ironman", "Captain America", "Thor", "Hulk", "Black Widow"]
+print(a[1:4])
---
+++ Day 5/string.py
@@ -0,0 +1,10 @@
+a = "Hello, World!"
+print(a)
+
+print(len(a))
+print(a.count("o"))
+print(a.index)
+print(a.center(50))
+
+#Slice string
+print(a[0:6])
